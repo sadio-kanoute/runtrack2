4bec0d3832b7a9597b8f5206393efc2e61a8ed93
sadio-kanoute <sadio.kanoute@laplateforme.io>
2025-07-21 12:02:33 +0200

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+// Partie 1 : Chaînes de caractères
+$str = "LaPlateforme";
+echo $str . "<br>";
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3 . "<br>";
+
+// Partie 2 : Variables numériques
+$val = 6;
+echo $val . "<br>";
+
+$val += 4;
+echo $val . "<br>";
+
+// Partie 3 : Booléens
+$myBool = true;
+echo $myBool . "<br>";
+
+$myBool = false;
+echo $myBool . "<br>";
+?>
